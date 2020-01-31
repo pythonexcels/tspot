cc0b47e60256dd84d449162eec1cbf6384d9de11
dlwhite <dlwhite@synopsys.com>
2020-01-30 22:28:20 -0800

    Refactor

--- doconv.sh
+++ doconv.sh
@@ -1,12 +1,13 @@
 
 # Convert all Markdown files in this hierarchy
 # to Docbook 5 XML
-for f in `find $PWD -name '*md'`
+for f in `find markdown -name '*md'`
 do
   d=`dirname $f`
   md=`basename $f`
   xml=`echo $md | sed s/\.md/\.xml/`
+  dita=`echo $xml | sed s/markdown/topics/`
   echo "Running pandoc on ${f} ..."
-  cd ${d}
-  pandoc -f gfm -t docbook5 -s -o "${xml}" "${md}"
+  # cd ${d}
+  pandoc -f gfm -t docbook5 -s -o "${d}/${xml}" "${d}/${md}"
 done
